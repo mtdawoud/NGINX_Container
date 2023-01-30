a440f265d141802ff110f5d36d73711bb06f09b6
mtdawoud <119929615+mtdawoud@users.noreply.github.com>
2023-01-30 07:30:23 +0100

    Update nginxinstall.sh

--- nginxinstall.sh
+++ nginxinstall.sh
@@ -8,5 +8,5 @@ sudo yum install git -y
 sudo amazon-linux-extras install nginx1
 sudo systemctl start nginx.service
 sudo chmod 777 -R /usr/share/nginx/html/
-sudo echo -e '<html>\n<html>\n\t<body>\n\t\t<h1>Hello CGI!</h1>\n\t</body>\n</html>' > /usr/share/nginx/html/index.html
+sudo echo -e '<html>\n<html>\n\t<body>\n\t\t<h1>Hello World!</h1>\n\t</body>\n</html>' > /usr/share/nginx/html/index.html
 sudo systemctl restart nginx.service
